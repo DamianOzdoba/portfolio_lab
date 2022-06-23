bf2ce9edaa124f1446266d7e4279e0d58e4d25f8
damian <damian@iMac.local>
2022-06-23 10:28:30 +0200

    structure of component

--- src/components/HomeWhoWeHelp.js
+++ src/components/HomeWhoWeHelp.js
@@ -3,7 +3,12 @@ import {Element} from "react-scroll";
 
 function HomeWhoWeHelp() {
     return (
-        <Element name='homeWhoWeHelp' className ="section">HomeWhoWeHelp</Element>
+        <Element name='homeWhoWeHelp' className="home-who-we-help">
+            <h2 className="title">Komu pomagamy?</h2>
+            <div className="home-who-we-help__btns">
+
+            </div>
+        </Element>
     );
 }
 
